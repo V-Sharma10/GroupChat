3c40875fa3e9b7968ba21ad36db3dcd0bee0ecce
VivekSharma <2017ugcs071@nitjsr.ac.in>
2019-10-12 22:07:07 +0530

    .

--- src/Components/Group.js
+++ src/Components/Group.js
@@ -1,13 +1,25 @@
 import React from 'react'
+import firebase from './../Firebase/firebase'
 
 
-export default function Group(props) {
+
+export default class Group extends React.Component {
+
+    componentDidMount(){
+        firebase.database().ref(`/rooms/${this.props.match.params.grpID}/messages`).on('value',(snapshot)=>{
+            console.log(snapshot.val());
+        })
+    }
+
+    render(){
+
     return (
         
         <div>{window.scrollTo(0,0)}
 
-            {props.match.params.grpID}
+           <h2 style={{textAlign:'center', color:'navy'}}> {this.props.match.params.grpID}</h2>
             
         </div>
     )
+}
 }
